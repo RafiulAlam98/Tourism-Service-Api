2809e8f6a9731700f032a211b455929bb7d64bbb
unknown <mrafiul.alam7@gmail.com>
2021-10-30 10:56:41 +0600

    new service by admin section added

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express()
 const port = process.env.PORT || 5000
 
 
+const ObjectId = require('mongodb').ObjectId
 app.use(cors())
 app.use(express.json())
 
@@ -24,17 +25,42 @@ const run = async() =>{
           const offerCollection = database.collection('offers') 
           const userCollection = database.collection('users')
 
+          // get offers from database
           app.get('/offers', async(req,res)=>{
                const query = offerCollection.find({})
                const result = await query.toArray()
                res.json(result)
           })
 
+          // post user information
           app.post('/users', async(req,res)=>{
                const newUser = req.body;
-               console.log(newUser)
                const result = await userCollection.insertOne(newUser)
-               console.log('hit the api')
+               res.json(result)
+          })
+
+          // get all user
+          app.get('/users', async(req,res)=>{
+               const query = userCollection.find({})
+               const result = await query.toArray()
+               res.json(result)
+          })
+
+
+          // delete 
+          app.delete('/users/:id',async(req,res)=>{
+               const id = req.params.id
+               console.log(id)
+               const query = {_id:ObjectId(id)}
+               const result = await userCollection.deleteOne(query)
+               res.json(result)
+          })
+
+
+          // add new service post method
+          app.post('/admin', async(req,res)=>{
+               const newService = req.body;
+               const result = await offerCollection.insertOne(newService) 
                res.json(result)
           })
 
